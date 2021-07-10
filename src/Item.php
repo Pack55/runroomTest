6b38312a1609cbfe112ffccff326296c662a33f0
packGarcia <pg@srjingles.com>
2021-07-10 23:35:41 +0200

    Created item interface.

--- src/Item.php
+++ src/Item.php
@@ -2,57 +2,9 @@
 
 namespace Runroom\GildedRose;
 
-class Item {
 
-    private $name;
-    private $sell_in;
-    private $quality;
-
-    const MAXQUALITY = 50;
-    const MINQUALITY = 0;
-    const AGEDBRIE = "Aged Brie";
-    const BACKSTAGEPASES = "Backstage passes to a TAFKAL80ETC concert";
-    const SULFURASHAND = "Sulfuras, Hand of Ragnaros";
-
-    function __construct($name, $sell_in, $quality) {
-        $this->name = $name;
-        $this->sell_in = $sell_in;
-        $this->quality = $quality;
-    }
-
-    public function __toString() {
-        return "{$this->name}, {$this->sell_in}, {$this->quality}";
-    }
-
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    public function getQuality()
-    {
-        return $this->quality;
-    }
-
-    public function setQuality($quality)
-    {
-        $this->quality = $quality;
-    }
-
-    public function getSellIn()
-    {
-        return $this->sell_in;
-    }
-
-    public function setSellIn($sell_in)
-    {
-        $this->sell_in = $sell_in;
-    }
+class Item extends ItemInterface
+{
 
     public function update()
     {
