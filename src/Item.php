1cea99514d76c5e4d4ffe0b815d434132180f4d6
packGarcia <pg@srjingles.com>
2021-07-10 23:12:42 +0200

    Changed Item attributes visibility.

--- src/Item.php
+++ src/Item.php
@@ -4,9 +4,9 @@ namespace Runroom\GildedRose;
 
 class Item {
 
-    public $name;
-    public $sell_in;
-    public $quality;
+    private $name;
+    private $sell_in;
+    private $quality;
 
     function __construct($name, $sell_in, $quality) {
         $this->name = $name;
@@ -16,6 +16,36 @@ class Item {
 
     public function __toString() {
         return "{$this->name}, {$this->sell_in}, {$this->quality}";
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getQuality()
+    {
+        return $this->quality;
+    }
+
+    public function setQuality($quality)
+    {
+        $this->quality = $quality;
+    }
+
+    public function getSellIn()
+    {
+        return $this->sell_in;
+    }
+
+    public function setSellIn($sell_in)
+    {
+        $this->sell_in = $sell_in;
     }
 
 }
